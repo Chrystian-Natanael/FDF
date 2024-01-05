54670cf8565464b4b3ffbae5e9fe14b20a180286
cnatanae <cnatanae@student.42sp.org.br>
2024-01-05 16:27:36 -0300

    Fists modifications

---
+++ includes/fdf.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fdf.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cnatanae <cnatanae@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/03 05:58:53 by cnatanae          #+#    #+#             */
+/*   Updated: 2024/01/05 15:41:27 by cnatanae         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FDF_H
+# define FDF_H
+
+# include <errno.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <stdio.h>
+# include <math.h>
+# include "../lib/libft/include/libft.h"
+# include "../lib/MLX42/include/MLX42/MLX42.h"
+
+# define WIDTH 640
+# define HEIGHT 640
+
+#endif
