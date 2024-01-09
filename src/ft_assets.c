90415f5a70b4b8587674e5318eece5ffc353da0b
cnatanae <cnatanae@student.42sp.org.br>
2024-01-09 15:15:32 -0300

    Added image to menu in left

--- src/ft_assets.c
+++ src/ft_assets.c
@@ -6,7 +6,7 @@
 /*   By: cnatanae <cnatanae@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 14:14:04 by cnatanae          #+#    #+#             */
-/*   Updated: 2024/01/09 15:04:06 by cnatanae         ###   ########.fr       */
+/*   Updated: 2024/01/09 15:14:55 by cnatanae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,26 @@
 
 void	ft_load_assets(t_data *data)
 {
+	int32_t	x;
+	int32_t	y;
+	int32_t	menu_width;
+
+	x = 0;
+	y = 0;
+	menu_width = 300;
+
 	data->canva.icon = mlx_load_png("./img/icon.png");
 	mlx_set_icon(data->mlx, data->canva.icon);
+	data->canva.menu = mlx_new_image(data->mlx, menu_width, HEIGHT);
+	mlx_image_to_window(data->mlx, data->canva.menu, 0, 0);
+	while (x < menu_width)
+	{
+		while (y < HEIGHT)
+		{
+			mlx_put_pixel(data->canva.menu, x, y, 0x404040FF);
+			y++;
+		}
+		y = 0;
+		x++;
+	}
 }
