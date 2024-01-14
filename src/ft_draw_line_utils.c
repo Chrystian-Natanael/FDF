7718b003051278550e08a97be15b20397c4b0cbf
cnatanae <cnatanae@student.42sp.org.br>
2024-01-14 09:50:45 -0300

    Fix buf forfun

--- src/ft_draw_line_utils.c
+++ src/ft_draw_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: cnatanae <cnatanae@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/14 02:47:18 by cnatanae          #+#    #+#             */
-/*   Updated: 2024/01/14 02:49:16 by cnatanae         ###   ########.fr       */
+/*   Updated: 2024/01/14 09:50:15 by cnatanae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,8 @@ void	ft_is_ff_p1(t_data *data, int32_t x, int32_t y)
 			ft_plot_line_xin(data, ft_projection(x, y, ft_get_z(x, y, data), \
 			data), ft_projection(x + 1, y, ft_get_z(x + 1, y, data), data));
 	}
-	else if ((ft_get_z(x, y, data) > 0) && data->forfun == 1)
+	else if (((ft_get_z(x,y, data) > 0)
+		|| (ft_get_z(x + 1,y, data) > 0)) && data->forfun == 1)
 	{
 		if (data->typeline == 1)
 			ft_plot_line(data, ft_projection(x, y, ft_get_z(x, y, data), \
@@ -45,7 +46,8 @@ void	ft_is_ff_p2(t_data *data, int32_t x, int32_t y)
 			ft_plot_line_xin(data, ft_projection(x, y, ft_get_z(x, y, data), \
 			data), ft_projection(x, y + 1, ft_get_z(x, y + 1, data), data));
 	}
-	else if ((ft_get_z(x, y, data) > 0) && data->forfun == 1)
+	else if (((ft_get_z(x,y, data) > 0)
+	|| (ft_get_z(x,y + 1, data) > 0)) && data->forfun == 1)
 	{
 		if (data->typeline == 1)
 			ft_plot_line(data, ft_projection(x, y, ft_get_z(x, y, data), \
